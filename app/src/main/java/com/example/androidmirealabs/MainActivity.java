cf3ed1c2996c48d0ecbfa2748212d2ef512d9092
platonso <plat100916@gmail.com>
2024-02-18 23:38:00 +0300

    commit pr2

--- app/src/main/java/com/example/androidmirealabs/MainActivity.java
+++ app/src/main/java/com/example/androidmirealabs/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.androidmirealabs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView textView;
     private Button button1;
-    private Button button2;
     private int counter = 0;
 
     @Override
@@ -19,12 +20,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Получение и вывод информации о версии Android SDK
+        int sdkVersion = Build.VERSION.SDK_INT;
+        String sdkInf =  "SDK Version: " + sdkVersion;
+        Toast.makeText(this, sdkInf, Toast.LENGTH_SHORT).show();
+
         textView = findViewById(R.id.tw);
         button1 = findViewById(R.id.btn1);
-        button2 = findViewById(R.id.btn2);
 
-        button1.setOnClickListener(this::onClick);
-        button2.setOnClickListener(this::onClick);
+        button1.setOnClickListener(this);
     }
 
     @Override
@@ -32,9 +36,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v.getId() == R.id.btn1) {
             counter++;
             textView.setText(String.valueOf(counter));
-        } else {
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-            startActivity(intent);
         }
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("key", counter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt("key");
+    }
+
+
 }
