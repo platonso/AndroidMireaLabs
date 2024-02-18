dc22bb383a77d1492b5fc2c9f2897bdb5598fab2
platonso <plat100916@gmail.com>
2024-02-18 22:09:08 +0300

    init

---
+++ app/src/main/java/com/example/androidmirealabs/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.androidmirealabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView textView;
+    private Button button1;
+    private Button button2;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.tw);
+        button1 = findViewById(R.id.btn1);
+        button2 = findViewById(R.id.btn2);
+
+        button1.setOnClickListener(this::onClick);
+        button2.setOnClickListener(this::onClick);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn1) {
+            counter++;
+            textView.setText(String.valueOf(counter));
+        } else {
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+        }
+    }
+}
