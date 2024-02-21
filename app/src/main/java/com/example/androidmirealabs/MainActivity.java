b8614db945780cb1a5b0464c471c02756f6d13d7
platonso <plat100916@gmail.com>
2024-02-21 23:28:42 +0300

    added markup in Java

--- app/src/main/java/com/example/androidmirealabs/MainActivity.java
+++ app/src/main/java/com/example/androidmirealabs/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.androidmirealabs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+        TextView textView = new TextView(this);
+        textView.setBackgroundColor(0xFA76AAA9);
+
+        textView.setText("Count");
+        textView.setTextSize(30);
+
+
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
+                (ViewGroup.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(430, 400, 60, 50);
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        textView.setLayoutParams(layoutParams);
+        textView.setPadding(20, 20, 20, 20);
+        constraintLayout.addView(textView);
+        setContentView(constraintLayout);
+
+
+
+
     }
 }
+
+
