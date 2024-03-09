2738f5e229c44f152fce6866f82fa9a6edcfb6a2
platonso <plat100916@gmail.com>
2024-03-10 00:40:23 +0300

    release

--- app/src/main/java/com/example/androidmirealabs/MainActivity.java
+++ app/src/main/java/com/example/androidmirealabs/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.androidmirealabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.image);
+        String filename = "custom_image.jpg";
+        try (InputStream inputStream =
+                     getApplicationContext().getAssets().open(filename)) {
+            Drawable drawable = Drawable.createFromStream(inputStream, null);
+            imageView.setImageDrawable(drawable);
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
