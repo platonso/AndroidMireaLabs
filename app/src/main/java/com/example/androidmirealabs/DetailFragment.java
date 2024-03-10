d2fcc75e3f7b9da4fc91884beb04580920319d75
platonso <plat100916@gmail.com>
2024-03-10 19:00:15 +0300

    взаимодействие между фрагментами

---
+++ app/src/main/java/com/example/androidmirealabs/DetailFragment.java
@@ -0,0 +1,24 @@
+package com.example.androidmirealabs;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+    // обновление текстового поля
+    public void setSelectedItem(String selectedItem) {
+        TextView view = getView().findViewById(R.id.detailsText);
+        view.setText(selectedItem);
+    }
+}
--- app/src/main/java/com/example/androidmirealabs/MainActivity.java
+++ app/src/main/java/com/example/androidmirealabs/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements
+        ListFragment.OnFragmentSendDataListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSendData(String selectedItem) {
+        DetailFragment fragment = (DetailFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.detailFragment);
+        if (fragment != null)
+            fragment.setSelectedItem(selectedItem);
     }
 }
