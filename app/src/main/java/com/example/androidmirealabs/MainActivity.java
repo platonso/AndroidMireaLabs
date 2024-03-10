22a573548257da72eaee5cdd78ad44c0bf4590a1
platonso <plat100916@gmail.com>
2024-03-11 02:17:46 +0300

    множественный выбор в списке

--- app/src/main/java/com/example/androidmirealabs/MainActivity.java
+++ app/src/main/java/com/example/androidmirealabs/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.androidmirealabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.androidmirealabs.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    String[] food = {"Бургер", "Паста", "Пицца", "Салат",
+            "Суши"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_multiple_choice, food);
+        // устанавливаем для списка адаптер
+        binding.foodList.setAdapter(adapter);
+        // добавляем для списка слушатель
+        binding.foodList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                SparseBooleanArray selected = binding.foodList.getCheckedItemPositions();
+                String selectedItems = "";
+                for (int i = 0; i < food.length; i++) {
+                    if (selected.get(i))
+                        selectedItems += food[i] + ",";
+                }
+                // установка текста элемента TextView
+                binding.selection.setText("Выбрано: " + selectedItems);
+            }
+        });
     }
 }
