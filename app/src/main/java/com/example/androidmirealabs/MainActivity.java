fcefba8d07f026e6ee10a8f0ef71f6d059292d7e
platonso <plat100916@gmail.com>
2024-04-01 02:57:57 +0300

    menu

--- app/src/main/java/com/example/androidmirealabs/MainActivity.java
+++ app/src/main/java/com/example/androidmirealabs/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.androidmirealabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        TextView headerView = findViewById(R.id.selectedMenuItem);
+        if (id == R.id.action_settings) {
+            headerView.setText("Настройки");
+            return true;
+        } else if (id == R.id.open_settings) {
+            headerView.setText("Открыть");
+            return true;
+        } else if (id == R.id.save_settings) {
+            headerView.setText("Сохранить");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
