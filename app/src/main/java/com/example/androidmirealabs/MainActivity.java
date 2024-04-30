12d9c48d3895e81cc074561ca85d611585034102
platonso <plat100916@gmail.com>
2024-04-30 21:29:29 +0300

    all done

--- app/src/main/java/com/example/androidmirealabs/MainActivity.java
+++ app/src/main/java/com/example/androidmirealabs/MainActivity.java
@@ -29,29 +29,29 @@ public class MainActivity extends AppCompatActivity {
         booksTextView = findViewById(R.id.booksTextView);
         deleteButton = findViewById(R.id.deleteButton);
 
-        // Установка обработчика нажатия на кнопку
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Удаление всех записей из базы данных
                 int rowsDeleted = deleteAllBooks();
                 if (rowsDeleted > 0) {
-                    Toast.makeText(MainActivity.this, "All books deleted successfully", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "All books deleted successfully",
+                            Toast.LENGTH_SHORT).show();
                     // Обновление отображения списка книг в TextView после удаления
                     displayBooks();
                 } else {
-                    Toast.makeText(MainActivity.this, "Failed to delete books", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Failed to delete books",
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-        // Отображение списка книг в TextView
         displayBooks();
     }
 
     // Метод для отображения списка книг в TextView
     private void displayBooks() {
-        Cursor cursor = getContentResolver().query(CONTENT_URI, null, null, null, null);
+        Cursor cursor = getContentResolver().query(CONTENT_URI, null, null,
+                null, null);
         if (cursor != null) {
             StringBuilder booksInfo = new StringBuilder();
             while (cursor.moveToNext()) {
@@ -59,10 +59,13 @@ public class MainActivity extends AppCompatActivity {
                 @SuppressLint("Range") String title = cursor.getString(cursor.getColumnIndex("title"));
                 @SuppressLint("Range") String author = cursor.getString(cursor.getColumnIndex("author"));
 
-                booksInfo.append("Book ID: ").append(id).append(", Title: ").append(title).append(", Author: ").append(author).append("\n");
+                booksInfo.append("Book ID: ")
+                        .append(id).append(", Title: ")
+                        .append(title).append(", Author: ")
+                        .append(author)
+                        .append("\n");
             }
             cursor.close();
-
             // Установка текста в TextView
             booksTextView.setText(booksInfo.toString());
         } else {
